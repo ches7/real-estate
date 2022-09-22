15e366d5dbec7dcbfa2af03dbf68d66f902c4e39
Connor Hesmondhalgh <chesmon1@hotmail.co.uk>
2022-09-22 13:31:14 +0100

    account page

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,33 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import { Link, useParams } from "react-router-dom";
 import NotFound from '../NotFound';
 import { AuthContext } from '../utils/AuthContext';
 import { useContext } from 'react';
+import PropertyCard from '../components/PropertyCard';
 
 function Account() {
 
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
+  const [userData, setUserData] = useState([]);
+  const [userError, setUserError] = useState(null);
+  const [properties, setProperties] = useState([])
 
-  const { user, loading, dispatch } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
+
 
   useEffect(() => {
     const fetchData = async () => {
       await axios.get(`http://localhost:8080/users/${user._id}`)
-        .then(res => { if (res.status !== 200) { throw Error('could not fetch the data for that resource') } else { setData(res.data); } })
-        .catch(err => { setError(err.message); setData(null) });
+        .then(res => { if (res.status !== 200) { throw Error('could not fetch the data for that resource') } else { setUserData(res.data); } })
+        .catch(err => { setUserError(err.message); setUserData(null) });
     }
     fetchData();
+
   }, [user]);
 
-  if (error) {
+  useEffect(() => {
+    if (userData.savedProperties) {
+      for (let i = 0; i < userData.savedProperties.length; i++) {
+        const fetchPropertyData = async () => {
+          await axios.get(`http://localhost:8080/properties/${userData.savedProperties[i]}`)
+            .then(res => {
+              if (res.status !== 200) { throw Error('could not fetch the data for that resource') }
+              else { setProperties(oldArray => [...oldArray, res.data]) }
+            })
+            .catch(err => { /*console.log(err)*/ });
+        }
+        fetchPropertyData();
+      }
+      console.log(properties);
+    }
+  }, [userData])
+
+  if (userError) {
     return (<NotFound />)
   } else
     return (
       <div>
-        {data.email}
+        <h1>Saved properties</h1>
+
+        <div>
+          {properties.map((p, i) => (
+            <PropertyCard key={i} price={p.price} title={p.title} location={p.location} description={p.description} id={p._id}
+              beds={p.beds} baths={p.baths} receptions={p.receptions} type={p.type} />
+          ))}
+        </div>
       </div>
+
+
     );
 }
 
--- client/src/pages/List.js
+++ client/src/pages/List.js
@@ -1,43 +1,44 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useLocation, Link } from 'react-router-dom'
+import { useState } from "react";
+import { useLocation } from 'react-router-dom'
 import Filters from "../components/Filters";
 import PropertyCard from "../components/PropertyCard";
 import useFetch from "../hooks/useFetch";
 
-    function List(){
-        const loc = useLocation();
-        const [location, setLocation] = useState(new URLSearchParams(loc.search).get('location'));
+function List() {
+    const loc = useLocation();
+    const [location, setLocation] = useState(new URLSearchParams(loc.search).get('location'));
 
-        // useEffect(() => {
-        //     const fetchData = async () => {
-        //     await axios.get(`http://localhost:8080/properties?location=${location}`)
-        //       .then(res => {if (res.status !== 200){throw Error('could not fetch the data for that resource')} else { setData(res.data); }})
-        //       .catch(err => {setError(err.message); setData(null)});
-        //     } 
-        //     fetchData();
-        //   }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //     await axios.get(`http://localhost:8080/properties?location=${location}`)
+    //       .then(res => {if (res.status !== 200){throw Error('could not fetch the data for that resource')} else { setData(res.data); }})
+    //       .catch(err => {setError(err.message); setData(null)});
+    //     } 
+    //     fetchData();
+    //   }, []);
 
-        const { data, loading, error } = useFetch(
-            `http://localhost:8080/properties?location=${location}`
-          );
+    const { data, loading, error } = useFetch(
+        `http://localhost:8080/properties?location=${location}`
+    );
 
-        return (
-            <div>
-                <Filters/>
-                <div className="d-flex justify-content-center">
+    //console.log(data)
+
+    return (
+        <div>
+            <Filters />
+            <div className="d-flex justify-content-center">
                 <h1>Property for sale in xyz</h1>
-                </div>
-                {/* <button><Link to={`/properties/create`}>Create new property</Link></button> */}
-                 <div>
-                    {data.map((p, i) => (
-                        <PropertyCard key={i} price={p.price} title={p.title} location={p.location} description={p.description} id={p._id} 
-                        beds={p.beds} baths={p.baths} receptions={p.receptions} type={p.type}/>
-                        // <li key={i}><Link to={`/properties/${p._id}`}>{p.location}</Link></li>
-                    ))}
-                </div> 
             </div>
-        );
+            {/* <button><Link to={`/properties/create`}>Create new property</Link></button> */}
+            <div>
+                {data.map((p, i) => (
+                    <PropertyCard key={i} price={p.price} title={p.title} location={p.location} description={p.description} id={p._id}
+                        beds={p.beds} baths={p.baths} receptions={p.receptions} type={p.type} />
+                    // <li key={i}><Link to={`/properties/${p._id}`}>{p.location}</Link></li>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default List;
