62d70c203636e29518b642c5de5b76e4d6dbd39a
ches7 <chesmon1@hotmail.co.uk>
2023-04-24 13:09:00 +0100

    docker backend fix

--- server/app.js
+++ server/app.js
@@ -11,39 +11,12 @@ import adminsRoute from "./routes/admins.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const MONGO_URI =  `${process.env.MONGO_URI}` || 'mongodb://127.0.0.1:27017/real-estate';
+mongoose.connect('mongodb://mongo:27017/real-estate');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/real-estate');
-
-
-// mongoose.connect(mongoAddress);
-
-// mongoose.connection.on("error", console.error.bind(console, "connection error:"));
-// mongoose.connection.once("open", () => {
-//     console.log("Database connected");
-// });
-
-// const options = {
-//     autoIndex: false, // Don't build indexes
-//     reconnectTries: 30, // Retry up to 30 times
-//     reconnectInterval: 500, // Reconnect every 500ms
-//     poolSize: 10, // Maintain up to 10 socket connections
-//     // If not connected, return errors immediately rather than waiting for reconnect
-//     bufferMaxEntries: 0
-//   }
-
-const connectWithRetry = () => {
-  console.log('MongoDB connection with retry')
-  mongoose.connect(MONGO_URI).then(()=>{
-    console.log('MongoDB is connected')
-  }).catch(err=>{
-    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
-    console.log(err);
-    setTimeout(connectWithRetry, 5000)
-  })
-}
-
-connectWithRetry()
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
 
